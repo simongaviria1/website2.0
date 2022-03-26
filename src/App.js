7ec2a4c0056792b7694fe905c107b1992fe772da
Simon Gaviria <simongaviria1@gmail.com>
2022-03-26 18:54:45 -0400

    fixed some styling

--- src/App.js
+++ src/App.js
@@ -63,35 +63,37 @@ const Fullpage = () => (
             </div>
           </div>
           <div id="resume-section" className="section">
-            <h1>Resume</h1>
-            <div id="work-container">
-              <div id="Google" className="resume-content-container">
-                Developer Advocate
-                <br />
-                Classroom, Google | APRIL 2019 - CURRENT
-                <br />
-                <p>
-                  <ul className="experience">
-                    <li>
-                      Worked with mobile developers and TPM's to create and
-                      maintain test automation for digital, web and physical
-                      products
-                    </li>
-                    <li>
-                      Worked alongside UI and Core development teams to
-                      understand test automation gaps and implement coverage
-                    </li>
-                    <li>
-                      Worked with embedded systems team to test make testing an
-                      integral part of the software development process
+            <div className="text-container">
+              <h1>Resume</h1>
+              <div id="work-container">
+                <div id="Google" className="resume-content-container">
+                  Developer Advocate
+                  <br />
+                  Classroom, Google | APRIL 2019 - CURRENT
+                  <br />
+                  <p>
+                    <ul className="experience">
                       <li>
-                        Created and implemented test suites for Android, iOS,
-                        Tread and Bike teams for release products
+                        Worked with mobile developers and TPM's to create and
+                        maintain test automation for digital, web and physical
+                        products
                       </li>
-                    </li>
-                  </ul>
-                </p>
-                <br />
+                      <li>
+                        Worked alongside UI and Core development teams to
+                        understand test automation gaps and implement coverage
+                      </li>
+                      <li>
+                        Worked with embedded systems team to test make testing
+                        an integral part of the software development process
+                        <li>
+                          Created and implemented test suites for Android, iOS,
+                          Tread and Bike teams for release products
+                        </li>
+                      </li>
+                    </ul>
+                  </p>
+                  <br />
+                </div>
               </div>
               <div id="peloton" className="resume-content-container">
                 SOFTWARE ENGINEER
